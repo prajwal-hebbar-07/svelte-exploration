5a3bf6b61b20fbeb29896b431e7e708ebb4fded3
Hebbar <prajwalhebbaras@gmail.com>
2025-03-29 16:00:04 +0530

    update main page with interactive button and dynamic content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+    function onclick() {
+        console.log("Hello from Prajwal on Button Click!")
+    }
+    let number1 = 1
+</script>
+
+<h1>{number1}</h1>
+<p>Hello From Prajwal!</p>
+<button {onclick}>Click Me!</button>
