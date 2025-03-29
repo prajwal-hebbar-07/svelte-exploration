e28de80adf17e763a48166f0b4754f20def9411e
Hebbar <prajwalhebbaras@gmail.com>
2025-03-29 17:54:28 +0530

    feat: add edit mode toggle for username input display

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,17 @@
 <script lang="ts">
 	let userName = $state('');
+	let isEditMode = $state(false);
 </script>
 
 {#snippet userInput()}
 	<h1>Your UserName:</h1>
-	<input type="text" bind:value={userName} />
-
-	<p>{userName}</p>
+	{#if isEditMode}
+		<input type="text" bind:value={userName} />
+	{:else}
+		<p>{userName}</p>
+	{/if}
 {/snippet}
 
 {@render userInput()}
+
+<button onclick={() => (isEditMode = !isEditMode)}>{isEditMode ? 'Save Changes' : 'Edit'}</button>
