eed33624cf45f738e88d7e4064c0101438d6e3c1
Hebbar <prajwalhebbaras@gmail.com>
2025-03-29 16:01:07 +0530

    style: enhance header appearance with custom styles

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,3 +8,11 @@
 <h1>{number1}</h1>
 <p>Hello From Prajwal!</p>
 <button {onclick}>Click Me!</button>
+
+<style>
+    h1 {
+        color: steelblue;
+        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+        padding: 1rem;
+    }
+</style>
