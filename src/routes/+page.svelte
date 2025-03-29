9835f7e1ebc8c6c2e24880baefca437ae7a591cf
Hebbar <prajwalhebbaras@gmail.com>
2025-03-29 16:53:27 +0530

    feat: replace number display with username input and binding

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,8 @@
 <script lang="ts">
-    let number = $state(0)
-    let udpateHintText = $derived.by(() => hintMessageUpdate(number))
-
-    function onclick() {
-        number = number + 1
-    }
-    function hintMessageUpdate(number: number) {
-        if(number === 0) {
-            return "Hey! Why don't you click on the button!"
-        } else if (number === 1) {
-            return "You have clicked exactly 1 time!"
-        } else {
-            return `You have clicked ${number} times!`
-        }
-    }
+    let userName = $state("")
 </script>
 
-<button {onclick}>Click Me!</button>
-<h1>{number}</h1>
+<h1>Your Username:</h1>
+<input type="text" bind:value={userName} />
 
-<!-- This below line is one of the ways of updating conditional data -->
-<!-- <p>{number === 0 ? "Hey! Why don't you click on the Button?" : `You have clicked ${number} times!`}</p> -->
- <p>{udpateHintText}</p>
-
-<style>
-    h1 {
-        color: blueviolet;
-        padding: 0 1rem;
-    }
-</style>
+<p>{userName}</p>
