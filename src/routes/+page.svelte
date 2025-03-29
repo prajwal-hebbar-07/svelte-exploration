f8fe48687319c380e26291298400b50f66823f5d
Hebbar <prajwalhebbaras@gmail.com>
2025-03-29 16:18:05 +0530

    feat: update hint text display with derived variable for button clicks

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
     let number = $state(0)
+    let udpateHintText = $derived(number === 0 ? "Hey! Why don't you click on the button" : `You have clicked ${number} times!`)
+
     function onclick() {
         number = number + 1
     }
@@ -9,7 +11,8 @@
 <h1>{number}</h1>
 
 <!-- This below line is one of the ways of updating conditional data -->
-<p>{number === 0 ? "Hey! Why don't you click on the Button?" : `You have clicked ${number} times!`}</p>
+<!-- <p>{number === 0 ? "Hey! Why don't you click on the Button?" : `You have clicked ${number} times!`}</p> -->
+ <p>{udpateHintText}</p>
 
 <style>
     h1 {
