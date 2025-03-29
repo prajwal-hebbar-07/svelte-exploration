83234c29a78c63c109d15652e82e309025cc4937
Hebbar <prajwalhebbaras@gmail.com>
2025-03-29 16:14:55 +0530

    feat: implement interactive button to increment displayed number

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,27 +1,16 @@
 <script lang="ts">
+    let number = $state(0)
     function onclick() {
-        console.log("Hello from Prajwal on Button Click!")
+        number = number + 1
     }
-    let number1 = 1
 </script>
 
-<h1>{number1}</h1>
-<p>Hello From Prajwal!</p>
-<div class="container">
-    <p>This is the First Part!</p>
-    <p>This is the Second Part!</p>
-</div>
 <button {onclick}>Click Me!</button>
+<h1>{number}</h1>
 
 <style>
     h1 {
-        color: steelblue;
-        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
-        padding: 1rem;
-    }
-    .container {
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
+        color: blueviolet;
+        padding: 0 1rem;
     }
 </style>
